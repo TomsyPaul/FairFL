34956257cce90dc145ddfe1e5d5ea4c035fe6649
Tomsy Paul <tomsypaul@gmail.com>
2024-11-13 11:05:37 +0530

    Modified Dockerfile for non conda and run.py

--- run.py
+++ run.py
@@ -13,8 +13,8 @@ def run(rank, size):
 #   pass
 def init_processes(rank, size, fn, backend='gloo'):
    """ Initialize the distributed environment. """
-   os.environ['MASTER ADDR'] = '172.16.89.5'
-   os.environ['MASTER PORT'] = '12321'
+#   os.environ['MASTER ADDR'] = '172.16.89.5'
+#   os.environ['MASTER PORT'] = '12321'
    dist.init_process_group(backend, rank=rank, world_size=size)
    fn(rank, size)
 if __name__ == "__main__":
@@ -24,17 +24,17 @@ if __name__ == "__main__":
 #   parser.add_argument('--rank', metavar='number', required=True,
 #                        help='the rank of the process')
 #   args = parser.parse_args()
-   rank=1
-
+   rank=0
+   init_processes(rank, size, run)
 
 #   processes = []
 #   for rank in range(size):
-   p = Process(target=init_processes, args=(rank, size, run))
-   p.start()
+#   p = Process(target=init_processes, args=(rank, size, run))
+#   p.start()
 #   processes.append(p)
 #   for p in processes:
 #       p.join()
-   torch.distributed.barrier()
+#   torch.distributed.barrier()
    print("Message - all processes crossed barrier - from Rank ",rank)
 
 
