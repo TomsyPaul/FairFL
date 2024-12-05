b06412ac4f6619afac7e4737244c10bb256deed2
Tomsy Paul <tomsypaul@gmail.com>
2024-12-05 11:47:46 +0530

    Introduced files-to-upload, to use in runscript..avoid rebuilding container..

--- runscript.sh
+++ runscript.sh
@@ -11,8 +11,11 @@ do
  then
    if [ $coding == 'Y' ]
    then
-      scp run.py tomsy@$ip:mydfl
-      ssh -n tomsy@$ip docker cp /home/tomsy/mydfl/run.py c$i:/workspace/run.py   
+   while read filename
+   do
+      scp $filename tomsy@$ip:mydfl
+      ssh -n tomsy@$ip docker cp /home/tomsy/mydfl/$filename c$i:/workspace/$filename
+   done < files-to-upload      
    fi   
    gnome-terminal --window -- bash -c "ssh -n tomsy@$ip docker exec c$i python run.py --rank=$i --size=$size; echo Output of $i; exec bash"   
  ((i++))     	
