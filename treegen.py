7f98385a8240569e63d6927e6a0a65016582d56c
Tomsy Paul <tomsypaul@gmail.com>
2025-01-21 15:38:11 +0530

    coding completed for arbitrary n

--- treegen.py
+++ treegen.py
@@ -1,3 +1,7 @@
+import networkx as nx 
+import matplotlib.pyplot as plt
+
+
 def largest_power_le(n):
     k=0
     while(n>=pow(2,k)):
@@ -7,10 +11,10 @@ def largest_power_le(n):
 def generate_tree(n,i):
   if(n<=3 and i==0):
     print("Error, n<3")
-    return []
+    return (0,[])
   else:
     if(n==4):
-      return [(i,i+1),(i+1,i+3),(i+2,i+3)]
+      return (i+3,[(i,i+1),(i+1,i+3),(i+2,i+3)])
 #    elif(n==5):
 #      return [(i,i+1),(i+1,i+3),(i+2,i+4),(i+4,i+3)]  
 #    elif(n==6):
@@ -20,12 +24,12 @@ def generate_tree(n,i):
     else:
       k=largest_power_le(n)
       if(k==n):
-        l1=generate_tree(n//2,i)
-        l2=generate_tree(n//2,i+n//2)
-        return l1+l2+[(n//2+i-1,i+n-1)]
+        r1,l1=generate_tree(n//2,i)
+        r2,l2=generate_tree(n//2,i+n//2)
+        return (n+i-1,l1+l2+[(n//2+i-1,i+n-1)])
       else:
         if(n-k<4):
-           l=generate_tree(k,i)
+           r,l=generate_tree(k,i)
            if(n-k == 1):
              l.remove((k+i-2,k+i-1))
              l+=[(k+i-2,k+i)]
@@ -37,18 +41,25 @@ def generate_tree(n,i):
              l+=[(k+i+1,k+i-1)]
            else:
              l.remove((k+i-3,k+i-1))
+             l.remove((k+i-2,k+i-1))
              l+=[(k+i-3,k+i)]
-             l+=[(k+i,k+i+2)]
-             l+=[(k+i-1,k+i+1)]
+             l+=[(k+i,k+i-1)]
+             
+             l+=[(k+i-2,k+i+1)]
              l+=[(k+i+1,k+i+2)]
-           return l
+             l+=[(k+i+2,k+i-1)]
+           return (r,l)
         else:
-           l1=generate_tree(k,i)   
-           l2=generate_tree(n-k,k+i)
-           return l1+l2+[(k-1,n-1)]  
+           r1,l1=generate_tree(k,i)   
+           r2,l2=generate_tree(n-k,k+i)
+           return r1,l1+l2+[(r2,r1)]  
+
           
 if __name__ == "__main__":
      n=input("Enter n")
-     t=generate_tree(int(n),0)
-     print(t)
-       
+     r,t=generate_tree(int(n),0)
+     print(r,t)
+#     for edge in t:
+#        print(edge[0],">",edge[1])
+        
+
