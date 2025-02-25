66d3bf4940a6fc1c3439db14b26695516c89149d
Tomsy Paul <tomsypaul@gmail.com>
2025-02-25 11:10:14 +0530

    Completed coding kld.py.. starting testing..

--- kld.py
+++ kld.py
@@ -24,6 +24,37 @@ import torchvision.models as models
 
 import socket
 
+nextadjustment=None
+
+def getnextadjustment(key):
+    newstring=key
+    while True:
+        newstring=str(int(sha256(newstring.encode('utf-8')).hexdigest(),16))
+        strlength=len(newstring)
+        for i in range(strlength-4):
+#           yield newstring[i:i+4]
+#            yield '0.'+newstring[i:i+4]
+            yield '0.'+newstring[i:i+4]
+        newstring=newstring[strlength-4:strlength]
+  
+def set_leaf_pair_adder(rank, size, aux):
+    with open('layout-up', newline='') as csvfile1:
+        btreedata1 = list(csv.reader(csvfile1))
+    edge_dest=[currentrow[1] for currentrow in btreedata1]
+    if str(rank) not in edge_dest:
+        aux["isleaf"]=True
+        if rank % 4 == 0:           
+           aux["adder"]=True
+           aux["partner"] = rank + 2
+        else:
+           aux["adder"]=False   
+           aux["partner"] = rank - 2        
+    else:
+        aux["isleaf"]=False    
+            
+
+
+
 
 #https://stackoverflow.com/questions/1908878/netcat-implementation-in-python
 def netcat(hostname, port, content):
@@ -33,22 +64,56 @@ def netcat(hostname, port, content):
 #    s.shutdown(socket.SHUT_WR)
 #    s.close()
 
-#def run(rank, size):
-#   """ Distributed function to be implemented later. """
-#   print("Rank = ", rank)
 def run(rank, size, epochs, K, averager, runid, roundid):
-    """ Distributed Synchronous SGD Example """
+    aux=dict(isleaf=False,partner=0,adder=False,key="1234567890")
+    set_leaf_pair_adder(rank, size, aux)
+    if aux["isleaf"] == True:
+            with open('keys', newline='') as csvfile4:
+                allkeys = list(csv.reader(csvfile4))
+                aux["key"]=allkeys[rank//4][0]
+            nextadjustment = getnextadjustment(aux["key"])
+
     local_counts=[]
     f1=open("local_counts","r")
     for i in f1.readlines():
        local_counts+=[int(i.strip())]
     f1.close()
+
+    additive = 0.0
+    if aux["isleaf"] == True:
+                if aux["adder"] == True:
+                    additive += float(next(nextadjustment))
+                else:
+                    additive -= float(next(nextadjustment))
+    sending_copy = [i+additive for i in local_counts]
+    global_counts = sending_copy.copy()
     
-    
-    
+    with open('layout-up', newline='') as csvfile1:
+        btreedata1 = list(csv.reader(csvfile1))
+    with open('layout-down', newline='') as csvfile2:
+        btreedata2 = list(csv.reader(csvfile2))
+
+#Tree Upward
+    for currentrow in btreedata1:
+                         if int(currentrow[0]) == rank:
+                           dist.send(tensor=sending_copy,dst=int(currentrow[1]))
+                           
+                         elif int(currentrow[1]) == rank:
+                           dist.recv(tensor=global_counts,src=int(currentrow[0]))
+                           sending_copy=[sending_copy[i]+global_counts[i] for i in range(len(sending_copy))]
+
+#Tree Downward
+    for currentrow in btreedata2:
+                        if int(currentrow[0]) == rank:
+                           dist.send(tensor=sending_copy,dst=int(currentrow[1]))
+                        elif int(currentrow[1]) == rank:
+                           dist.recv(tensor=global_counts,src=int(currentrow[0]))
+                           sending_copy=global_counts
+#           dist.all_reduce(param.grad.data, op=dist.reduce_op.SUM, group=0)
+    global_counts=[int(sending_copy[i]) for i in range(len(sending_copy))]
     
     coordinator="172.16.64.126"
-    netcat(coordinator,23532,f"Rank={rank}, local_counts={local_counts}\n".encode("utf-8"))
+    netcat(coordinator,23632,f"Rank={rank}, global_counts={global_counts}\n".encode("utf-8"))
 
 def init_processes(rank, size, epochs, K, averager, runid, fn, roundid, backend='gloo'):
    """ Initialize the distributed environment. """
--- runscript.sh
+++ runscript.sh
@@ -10,10 +10,28 @@ runid=$5
 >nc_output.txt
 nc -k -u -l 23432  >> nc_output.txt&
 
+>nc_local_counts.txt
+nc -k -u -l 23532  >> nc_local_counts.txt&
+
+>nc_global_counts.txt
+nc -k -u -l 23632  >> nc_global_counts.txt&
+
+python3 treegen.py --n=$size
+
+>keys
+keycount=`echo "$size/4" |bc`
+for((i=0;i<$keycount;i++))
+   do
+     echo "$RANDOM" >> keys
+   done
+
 #set files to upload
 >files-to-upload
-#echo keys >> files-to-upload
+echo layout-up >> files-to-upload
+echo layout-down >> files-to-upload
+echo keys >> files-to-upload
 echo find_local_count.py >> files-to-upload
+echo kld.py >> files-to-upload
 #echo run.py >> files-to-upload
 echo partition_sizes >> files-to-upload
 
@@ -49,6 +67,24 @@ do
     sleep 1
     echo "nc_local_counts = `cat nc_local_counts.txt | wc -l`"
 done
-#bash close-all-terminals.sh
+
+bash close-all-terminals.sh
+
+i=0
+while  read ip
+do
+     if [ ! -z $ip ]
+     then
+        gnome-terminal --window -- bash -c "ssh -n tomsy@$ip docker exec c$i python kld.py --rank=$i --size=$size --epochs=$epochs --averager=$averager --K=$K --runid=$runid --round=0; echo Output of $i; exec bash"   
+        ((i++))     	
+     fi
+done < hostips
 
 
+
+while [[ `cat nc_global_counts.txt | wc -l` < $size ]]
+do
+    sleep 1
+    echo "nc_global_counts = `cat nc_global_counts.txt | wc -l`"
+done
+bash close-all-terminals.sh
