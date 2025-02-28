0fcccdc3cc9d1bc680ed8c7eba9190d424edd4d3
Tomsy Paul <tomsypaul@gmail.com>
2025-02-28 15:23:13 +0530

    Apply to all with ampersand for parallel execution

--- runscript.sh
+++ runscript.sh
@@ -115,7 +115,7 @@ do
    echo layout-down >> files-to-upload
    #echo secrets >> files-to-upload
    echo keys >> files-to-upload
-   #echo run.py >> files-to-upload
+   echo run.py >> files-to-upload
    echo partition_sizes >> files-to-upload
    
    currentworldsize=`grep -o "," tempfile$x | wc -l`
