05c5d80fb4ba89ced3e33bbbc7559e9860c6467c
Tomsy Paul <tomsypaul@gmail.com>
2025-05-20 12:54:35 +0530

    KL runscript edited, removed partition_sizes

--- runscript.sh
+++ runscript.sh
@@ -7,7 +7,6 @@ averager=$3
 K=$4
 runid=$5
 
-cp partition_sizes partition_sizes_original
 cp indicesfile indicesfile_original
 
 >nc_output.txt
@@ -23,35 +22,12 @@ nc -k -u -l 23632  >> nc_result.txt&
 >files-to-upload
 echo find_local_count.py >> files-to-upload
 echo divergence.py >> files-to-upload
-echo partition_sizes >> files-to-upload
 echo run.py >> files-to-upload
 echo indicesfile >> files-to-upload
 
->partition_sizes
-for((i=0;i<$size;i++))
-do
- common=`echo 1.0/$size | bc -l`
-# common=`echo 1.0/16 | bc -l`
- echo -n "$common, ">>partition_sizes 
-done   
 
-#>indicesfile
-#DATASIZE=60000
-#for((i=0;i<$size;i++))
-#do
-# common=`echo $DATASIZE/$size | bc`
-# for((j=0;j<$common;j++))
-# do
-#  # common=`echo 1.0/16 | bc -l`
-#  data=`echo $i*$common+$j | bc -l`
-#  echo -n "$data,">>indicesfile 
-# done
-#echo "">>indicesfile 
-#done   
 
 tar -cvzf files-to-upload.gz -T files-to-upload
-#echo keys >> files-to-upload
-#echo partition_sizes >> files-to-upload
 
 cumulative_size=0
 
@@ -85,8 +61,6 @@ do
     sleep 1
     echo "nc_local_counts = `cat nc_local_counts.txt | wc -l`"
 done
-#read
-#bash close-all-terminals.sh
 
 i=0
 while  read ip
@@ -106,8 +80,6 @@ do
     echo "nc_result = `cat nc_result.txt | wc -l`"
 done
 
-
-
 sort -n -t"," -k2 nc_result.txt > sorted_result.txt
 cat sorted_result.txt | cut -d"," -f2 > testout
 for i in `cut sorted_result.txt -d"," -f1`
@@ -123,14 +95,12 @@ epochs=`echo $epochs/$rounds | bc`
 for((x=0;x<rounds;x++))
 do
 
-   cp tempfile$x partition_sizes
    cp indicesfile$x indicesfile
    
    >files-to-upload
-   echo partition_sizes >> files-to-upload
    echo indicesfile >> files-to-upload
    
-   currentworldsize=`grep -o "," tempfile$x | wc -l`
+   currentworldsize=`cat indicesfile | wc -l`
    head -n $currentworldsize hostips_sorted > selected_from_sorted
    
    cumulative_size=$((cumulative_size+currentworldsize))
@@ -175,7 +145,6 @@ do
       sleep 2
       echo "nc_output count = `cat nc_output.txt | wc -l`"
    done
-#   read
    
    echo "$currentworldsize,$2,$3,$K,$runid,$x" > "results/$currentworldsize-$averager-$epochs-$runid-$x"
    echo -e "******************\n" >> "results/$currentworldsize-$averager-$epochs-$runid-$x"
@@ -192,7 +161,6 @@ do
    echo "" >> "results/summary"
 done
 
-cp partition_sizes_original partition_sizes
 cp indicesfile_original indicesfile
 
 bash close-all-terminals.sh
